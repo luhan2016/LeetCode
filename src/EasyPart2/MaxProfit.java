5a18b2a558ef8b33e524ce6b34eaab2050c32e4f
luhan2016 <luhansophia@gmail.com>
2018-02-08 16:52:54 +0100

    Input: [7, 1, 5, 3, 6, 4] Output: 5  max. difference = 6-1 = 5 (not 7-1 = 6, as selling price needs to be larger than buying price)
    Input: [7, 1, 5, 3, 6, 4]
    Output: 5
    
    max. difference = 6-1 = 5 (not 7-1 = 6, as selling price needs to be larger than buying price)

--- src/EasyPart2/MaxProfit.java
+++ src/EasyPart2/MaxProfit.java
@@ -1,5 +1,14 @@
 package EasyPart2;
 
 public class MaxProfit {
-
+	public int maxProfit(int[] prices) {
+		int maxprofit=0, minprice=Integer.MAX_VALUE;
+		for (int i=0; i<prices.length;i++) {
+			if(minprice>prices[i])
+				minprice = prices[i];
+			else if(prices[i]-minprice > maxprofit)
+				maxprofit = prices[i]-minprice;
+		}
+		return maxprofit;
+	}
 }
