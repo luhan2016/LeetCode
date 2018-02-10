01592be1ba7aa0db48bb0bfa726a417a0f694b9b
luhan2016 <luhansophia@gmail.com>
2018-02-10 18:50:10 +0100

    rotate array
    rotate array

---
+++ src/Rotate.java
@@ -0,0 +1,19 @@
+
+public class Rotate {
+    public void rotate(int[] nums, int k) {
+        int tmp[] = new int[nums.length];
+        for(int i=0; i<nums.length; i++)
+        	tmp[i] = nums[(i+k)%nums.length];
+    	nums = tmp;
+    	for(int i=0; i<nums.length;i++)
+    		System.out.println(nums[i]);
+    }
+    
+    public static void main(String args[]) {
+    	int nums[] = new int[7];
+    	for(int i=0; i<7; i++)
+    		nums[i] = i+1;
+    	Rotate r = new Rotate();
+    	r.rotate(nums, 3);
+    }
+}
