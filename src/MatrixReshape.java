c7eea686051483378c8bb64ab7760e91c8dab8de
luhan2016 <luhansophia@gmail.com>
2018-02-12 11:20:08 +0100

    In MATLAB, there is a very useful function called 'reshape', which can reshape a matrix into a new one with different size but keep its original data
    In MATLAB, there is a very useful function called 'reshape', which can reshape a matrix into a new one with different size but keep its original data

---
+++ src/MatrixReshape.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+public class MatrixReshape {
+    public int[][] matrixReshape(int[][] nums, int r, int c) {
+    	int newNums[][] = new int[r][c];
+        for(int i=0; i<nums.length; i++) 
+        	for(int j=0; j<nums[0].length; j++) 
+    			newNums[(nums[0].length*i+j)/c][(nums[0].length*i+j)%c] = nums[i][j];
+    	return newNums;
+    }
+    
+    public static void main(String args[]) {
+    	
+    	int array1[][] = new int[4][1];
+    	int array2[][] = new int[2][2];
+        for(int i=0; i<array1.length; i++) 
+        	for(int j=0; j<array1[0].length; j++) 
+    			array1[i][j]=array1[0].length*i+j+1;
+    	MatrixReshape fP = new MatrixReshape();
+    	array2 = fP.matrixReshape(array1, 2, 2);
+    	for(int i=0; i<2;i++)
+    		for(int j=0; j<2;j++)
+    		System.out.println(array2[i][j]);
+    }
+}
